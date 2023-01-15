ec054e3204e366d86b75e4048f806558fdac7e50
james <jamesmcunningham98@gmail.com>
2023-01-15 18:47:23 +0000

    first

---
+++ serahcba/main.js
@@ -0,0 +1,57 @@
+import { coding } from '/serahcba/array.js'
+
+
+window.onload = printCode;
+
+const card = document.getElementById("card")
+
+card.addEventListener("click", flipCard);
+function flipCard() {
+    card.classList.toggle("flipCard");
+}
+
+
+
+
+function randomInt(max) {
+
+    return Math.floor(Math.random() * max);
+}
+
+function randomPick(array) {
+    return array[randomInt(array.length)]
+}
+
+randomPick(coding)
+
+
+// console.log(randomPick(coding));
+
+
+var item = randomPick(coding)
+
+// console.log(item.id);
+// console.log(item);
+// console.log(item.Code);
+// console.log(item.Description);
+// console.log(item.Title);
+
+
+
+
+function printCode() {
+    document.getElementById("title").innerHTML = (item.Title);
+    document.getElementById("desc").innerHTML = (item.Description);
+    document.getElementById("code").innerHTML = (item.Code);
+};
+
+
+let button = document.querySelector("#myButton");
+button.addEventListener("click", updateArray);
+function updateArray() {
+    var item = randomPick(coding)
+
+    document.getElementById("title").innerHTML = (item.Title);
+    document.getElementById("desc").innerHTML = (item.Description);
+    document.getElementById("code").innerHTML = (item.Code);
+} 
